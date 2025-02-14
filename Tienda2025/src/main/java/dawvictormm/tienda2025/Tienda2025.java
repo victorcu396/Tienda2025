83feffef9076560d40c87596f18c4cae1037111d
Victor <vmenendezmata@gmail.com>
2025-02-14 08:59:54 +0100

    MEJORA 2 en la tienda

---
+++ Tienda2025/src/main/java/dawvictormm/tienda2025/Tienda2025.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package dawvictormm.tienda2025;
+
+/**
+ *
+ * @author alu16d
+ */
+public class Tienda2025 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
