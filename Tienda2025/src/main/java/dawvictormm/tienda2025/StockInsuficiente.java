73500fdb2f857cf551b8a9667c1a42a1e45077e8
Victor <vmenendezmata@gmail.com>
2025-02-14 09:11:54 +0100

    Principio de Tienda2025

---
+++ Tienda2025/src/main/java/dawvictormm/tienda2025/StockInsuficiente.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dawvictormm.tienda2025;
+/**
+ *
+ * @author alu16d
+ */
+public class StockInsuficiente extends Exception{
+
+    public StockInsuficiente(String cadena) {
+        super(cadena);
+    }
+    
+}
